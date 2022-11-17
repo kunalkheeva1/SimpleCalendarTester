9b5424f349be456e29f23e082104117a44d5bfc3
Kunal Kheeva <98793512+kunalkheeva1@users.noreply.github.com>
2022-11-17 00:28:34 -0800

    all classes ready to debug

--- src/SimpleCalendarTester.java
+++ src/SimpleCalendarTester.java
@@ -1,5 +1,8 @@
-public class SimpleCalendarTester {
-    public static void main(String[] args) {
+import java.io.IOException;
+import java.text.ParseException;
 
+public class SimpleCalendarTester {
+    public static void main(String[] args) throws ClassNotFoundException, IOException, ParseException {
+        new GUI();
     }
 }
